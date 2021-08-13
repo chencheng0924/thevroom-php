975039d564a0daf2c2fa21167fa4db513bba087c
陳瑋呈 <chenweicheng@chenweichengs-MacBook-Air.local>
2021-08-13 13:59:25 +0800

    討論留言優化

--- forumreply.php
+++ forumreply.php
@@ -4,7 +4,7 @@
        $db_host = "127.0.0.1";
        $db_user = "root";
        $db_pass = "root";   // 記得更改自己的密碼
-       $db_select = "mydb";   // 記得更改成團專的schemas名稱
+       $db_select = "thevroom";   // 記得更改成團專的schemas名稱
 
        //建立資料庫連線物件
        $dsn = "mysql:host=".$db_host.";dbname=".$db_select;
